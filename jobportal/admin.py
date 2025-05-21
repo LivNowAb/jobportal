2f491c2dda98f8ed8f19ce7d1f886f5b3c766a24
nicoletaudy <taudy.n@gmail.com>
2025-05-21 20:56:35 +0200

    features: views, urls, templates

--- jobportal/admin.py
+++ jobportal/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from jobportal.models import Client, Advertisement, Position, District, Region, BusinessType
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Advertisement)
+admin.site.register(Position)
+admin.site.register(District)
+admin.site.register(Region)
+admin.site.register(BusinessType)
--- jobportal/views.py
+++ jobportal/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, "index.html")
+
+def ad_list(request):
+    return render(request, "advertisment/index.html")
+
+def client_list(request):
+    return render(request, "client/index.html")
