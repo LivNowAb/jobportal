4890b367af46708a95f4b7ef7f91beb878ea60b0
nicoletaudy <taudy.n@gmail.com>
2025-06-06 16:31:30 +0200

    Adjustment of class order

--- jobportal/forms.py
+++ jobportal/forms.py
@@ -7,50 +7,6 @@ from django.forms.fields import EmailField, ImageField
 from .models import Response, Advertisement, Position, Client, BusinessType, District
 
 
-class AdCreation(forms.ModelForm):
-    class Meta:
-        model = Advertisement
-        fields = ["heading", "position", "text", "salary"]
-
-    heading = CharField(label="Název", max_length=128)
-    position = ModelChoiceField(label="Pracovní Pozice", queryset=Position.objects,
-                                widget=forms.Select(attrs={"class": "form-control"}))
-    text = CharField(label="Obsah", widget=Textarea(attrs={"class": "form-control", "cols": 40, "rows": 3}),
-                     required=True)
-    salary = CharField(label="Mzda", max_length=250)
-
-
-class ClientCreation(forms.ModelForm):
-    class Meta:
-        model = Client
-        fields = ["business_name", "business_type", "VAT_number", "address", "city", "district", "contact_email",
-                  "contact_phone", "logo"]
-
-    business_name = CharField(label="Název podniku", max_length=250)
-    business_type = ModelChoiceField(label="Typ provozovny", queryset=BusinessType.objects,
-                                widget=forms.Select(attrs={"class": "form-control"}))
-    VAT_number = CharField(label="IČO/DIČ", max_length=25, required=True)
-    address = CharField(label="Adresa provozovny", max_length=250)
-    city = CharField(label="Město", max_length=250)
-    district = ModelChoiceField(label="Okres", queryset=District.objects,
-                                widget=forms.Select(attrs={"class": "form-control"}))
-    contact_email = EmailField(label="Kontaktní e-mail provozovny")
-    contact_phone = CharField(label="Kontaktní telefonní číslo provozovny", max_length=128)
-    logo = ImageField(label="Logo provozovny", required=False)
-
-
-class ResponseForm(forms.ModelForm):
-    class Meta:
-        model = Response
-        fields = ['name', 'email', 'message', 'cv']
-        labels = {
-            'name': 'Jméno a příjmení',
-            'email': 'E-mail',
-            'message': 'Vaše zpráva',
-            'cv': 'Přiložit CV',
-        }
-
-
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(
         required=True,
@@ -82,4 +38,48 @@ class RegistrationForm(UserCreationForm):
             "username": forms.TextInput(attrs={
                 "class": "form-control"
             }),
+        }
+
+
+class ClientCreation(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = ["business_name", "business_type", "VAT_number", "address", "city", "district", "contact_email",
+                  "contact_phone", "logo"]
+
+    business_name = CharField(label="Název podniku", max_length=250)
+    business_type = ModelChoiceField(label="Typ provozovny", queryset=BusinessType.objects,
+                                widget=forms.Select(attrs={"class": "form-control"}))
+    VAT_number = CharField(label="IČO/DIČ", max_length=25, required=True)
+    address = CharField(label="Adresa provozovny", max_length=250)
+    city = CharField(label="Město", max_length=250)
+    district = ModelChoiceField(label="Okres", queryset=District.objects,
+                                widget=forms.Select(attrs={"class": "form-control"}))
+    contact_email = EmailField(label="Kontaktní e-mail provozovny")
+    contact_phone = CharField(label="Kontaktní telefonní číslo provozovny", max_length=128)
+    logo = ImageField(label="Logo provozovny", required=False)
+
+
+class AdCreation(forms.ModelForm):
+    class Meta:
+        model = Advertisement
+        fields = ["heading", "position", "text", "salary"]
+
+    heading = CharField(label="Název", max_length=128)
+    position = ModelChoiceField(label="Pracovní Pozice", queryset=Position.objects,
+                                widget=forms.Select(attrs={"class": "form-control"}))
+    text = CharField(label="Obsah", widget=Textarea(attrs={"class": "form-control", "cols": 40, "rows": 3}),
+                     required=True)
+    salary = CharField(label="Mzda", max_length=250)
+
+
+class ResponseForm(forms.ModelForm):
+    class Meta:
+        model = Response
+        fields = ['name', 'email', 'message', 'cv']
+        labels = {
+            'name': 'Jméno a příjmení',
+            'email': 'E-mail',
+            'message': 'Vaše zpráva',
+            'cv': 'Přiložit CV',
         }
