fa575ace6f90f365d2326c506713cdb64759dbfe
LivNowAb <livia.abdul@yahoo.com>
2025-06-23 18:26:23 +0200

    .env plus settings update

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c^xe73mb0b*-_dvk%h79o)=ix%ywl0w6!z(gcb)f*-9p+9k_7_'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
